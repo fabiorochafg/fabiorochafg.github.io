4152c3273ecf87fc60cf6e97c394c00fcf6124ba
Fábio Rocha <fabiorochafg@gmail.com>
2013-07-08 17:05:16 -0300

    Adding visible links to portfolio.

--- files/main.js
+++ files/main.js
@@ -2,10 +2,6 @@
 
 $(document).ready(function() {
 	$("#header, #header > div").css("height", $(window).height());
-	$(".preview-bluray-dvd-cd-book-collection .boxes li a").css("line-height", $(".preview-bluray-dvd-cd-book-collection > div").height()+"px");
-	$(".preview-html-blank-template .boxes li a").css("line-height", $(".preview-html-blank-template > div").height()+"px");
-	$(".preview-joomla-3-blank-template .boxes li a").css("line-height", $(".preview-joomla-3-blank-template > div").height()+"px");
-
 
 	/* Menu */
 	$('#menu a').on('click',function (e) {
@@ -18,9 +14,6 @@ $(document).ready(function() {
 	        window.location.hash = target;
 	    });
 	});
-
-	/* Portfolio */
-	$('#portfolio .box > div').each( function() { $(this).hoverdir(); } );
 });
 
 /* Google Analyctis */
@@ -28,6 +21,5 @@ $(document).ready(function() {
 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
 ga('create', 'UA-41913842-1', 'fabiorochafg.github.io');
 ga('send', 'pageview');
