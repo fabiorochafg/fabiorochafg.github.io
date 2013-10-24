9c32856083732e85f06507a6e27eee85ecbbe2fb
Fábio Rocha <fabiorochafg@gmail.com>
2013-10-24 10:34:04 -0300

    Adding HTML5 tags.

--- files/main.js
+++ files/main.js
@@ -1,11 +1,11 @@
 /* JS Document - Developed by Fabio Rocha (http://fabiorochafg.github.io | fabiorochafg@gmail.com) */
 
 $(document).ready(function() {
-	$("#header, #header > div").css("height", $(window).height());
+	$("header, header > div").css("height", $(window).height());
 
 	/* Menu */
-	$('#menu li:not(.language) a').on('click',function (e) {
-		$("#menu a").removeClass("active");
+	$('nav li:not(.language) a').on('click',function (e) {
+		$("nav a").removeClass("active");
 		$(this).toggleClass("active");
 	    e.preventDefault();
 	    var target = this.hash,
@@ -18,16 +18,16 @@ $(document).ready(function() {
 	});
 	$(window).scroll(function() {
 		if ($(this).scrollTop() == 0) {
-			$("#menu a").removeClass("active");
-			$("#header p").removeClass("opacity");
+			$("nav a").removeClass("active");
+			$("header p").removeClass("opacity");
 		} else {
-			$("#header p").addClass("opacity");
+			$("header p").addClass("opacity");
 		}
         if ($(this).scrollTop() <= $(window).height()-50) {
-        	$("#menu").removeClass("menufixo");
-            $("#menu").css("bottom", $(this).scrollTop());
+        	$("nav").removeClass("menufixo");
+            $("nav").css("bottom", $(this).scrollTop());
         } else {
-            $("#menu").addClass("menufixo");
+            $("nav").addClass("menufixo");
             
         }
     });
